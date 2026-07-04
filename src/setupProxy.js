7a6c2b4e6a79fd08d95fe1898338fba2b8ea48d5
Mohit Singh <mohit310ggn@gmail.com>
2026-07-04 23:23:43 +0530

    fix405

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080',
+      target: 'https://resume-mailer-438v.onrender.com',
       changeOrigin: true,
     })
   );
